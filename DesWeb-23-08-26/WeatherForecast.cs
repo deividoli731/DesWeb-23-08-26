e602bfaf0bd298c1fc945def9c37eb8f0e27f457
Deivid <deividoliveira0731@gmailcom>
2026-08-23 18:30:53 -0300

    Adicionar arquivos de projeto.

---
+++ DesWeb-23-08-26/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DesWeb_23_08_26
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
